186e7c5809c60c79c2689f00677b0bd7616a854b
Sondre Søråsdekkan <sondre.sorasdekkan@gmail.com>
2021-04-12 18:49:24 +0200

    add functions to serialize/deserialize users in Oauth

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+require("./models/User");
 require("./services/passport");
+
 const authRoutes = require("./routes/authRoutes");
 
 mongoose.connect(process.env.MONGO_URI, {
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 require("dotenv").config();
+const mongoose = require("mongoose");
+
+const User = mongoose.model("User");
+passport.serializeUser((user, done) => {
+  console.log(user);
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +23,22 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, Profile, done) => {
-      console.log("accessToken:" + accessToken);
-      console.log("RefreshToken: ", refreshToken);
-      console.log("Profile:", Profile);
+      User.findOne({ googleId: Profile.id })
+        .then((exisitingUser) => {
+          if (exisitingUser) {
+            done(null, exisitingUser);
+          } else {
+            //Create a new user record
+            new User({
+              googleId: Profile.id,
+            })
+              .save()
+              .then((user) => done(null, user));
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
   )
 );
