221c07525e6d55a78e9a922f7e74acbd552e4c16
Sondre Søråsdekkan <sondre.sorasdekkan@gmail.com>
2021-04-12 12:14:41 +0200

    Setup for dotenv

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+require("dotenv").config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 
