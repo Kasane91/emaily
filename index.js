2e732ab1ab54c6869c00f05ea2b2137fa1ab283c
Sondre Søråsdekkan <sondre.sorasdekkan@gmail.com>
2021-04-12 15:06:58 +0200

    Initial setup for mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 require("./services/passport");
 const authRoutes = require("./routes/authRoutes");
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 
