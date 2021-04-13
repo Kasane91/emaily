7e541ea92fab826c769793056194fefe79a0f001
Sondre Søråsdekkan <sondre.sorasdekkan@gmail.com>
2021-04-13 12:03:09 +0200

    initial react-router setup

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+
+const Header = () => <h2>HEADER</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>LANDING</h2>;
 
 const App = (props) => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Router>
+        <div>
+          <Header />
+          <Route path="/" exact component={Landing} />
+          <Route path="/surveys/new" component={SurveyNew} />
+          <Route path="/surveys" exact component={Dashboard} />
+        </div>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
